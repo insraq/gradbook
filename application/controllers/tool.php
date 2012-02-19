33ce26be45f56cce4f938bddced6f27af57aa5b5
insraq <ruoysun@gmail.com>
2012-02-19 20:54:05 +0800

    tool

---
+++ application/controllers/tool.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function resize($size)
+	{
+		$users = R::find('user');
+
+		foreach ($users as $u)
+		{
+			if (!empty($u->photo))
+			{
+				$config['image_library'] = 'gd2';
+				$config['source_image'] = './upload/' . $u->photo;
+				$config['new_image'] = './upload/thumb_' . $u->photo;
+				$config['maintain_ratio'] = TRUE;
+				$config['width'] = 220;
+				$config['height'] = 220;
+				$this->load->library('image_lib', $config);
+				$this->image_lib->resize();
+			}
+
+		}
+
+		$this->load->view('header');
+		$this->load->view('message', array('message' => '更改大小成功。'));
+		$this->load->view('footer');
+	}
+}
