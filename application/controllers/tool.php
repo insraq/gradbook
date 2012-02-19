acd283fd2e89f27da7b26996182a0e1062512510
insraq <ruoysun@gmail.com>
2012-02-19 21:01:53 +0800

    fix bug

--- application/controllers/tool.php
+++ application/controllers/tool.php
@@ -4,26 +4,28 @@ class Tool extends CI_Controller {
 
 	public function resize($size)
 	{
-		$users = R::find('profile');
+		$profile = R::find('profile');
+		$message = '';
 
-		foreach ($users as $u)
+		foreach ($profile as $p)
 		{
-			if (!empty($u->photo))
+			if (!empty($p->photo))
 			{
 				$config['image_library'] = 'gd2';
-				$config['source_image'] = './upload/' . $u->photo;
-				$config['new_image'] = './upload/thumb_' . $u->photo;
+				$config['source_image'] = './upload/' . $p->photo;
+				$config['new_image'] = './upload/thumb_' . $p->photo;
 				$config['maintain_ratio'] = TRUE;
 				$config['width'] = $size;
 				$config['height'] = $size;
 				$this->load->library('image_lib', $config);
 				$this->image_lib->resize();
+				$message .= $p->photo . ', ';
 			}
 
 		}
 
 		$this->load->view('header');
-		$this->load->view('message', array('message' => '更改大小成功。'));
+		$this->load->view('message', array('message' => $message . '更改大小成功。'));
 		$this->load->view('footer');
 	}
 }
