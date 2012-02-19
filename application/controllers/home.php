fff36780774e7f240a261b99bedd5e262929b90b
insraq <ruoysun@gmail.com>
2012-02-19 22:57:28 +0800

    fix bug

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -16,4 +16,19 @@ class Home extends CI_Controller {
 		));
 		$this->load->view('footer');
 	}
+
+	public function filter($item, $value)
+	{
+		$user = $this->login->require_login();
+
+		$this->load->helper('astro');
+		$users = R::find('profile', "`{$item}` = ?", array(urldecode($value)));
+		$this->load->view('header');
+		$this->load->view('home/home', array(
+			'users' => $users,
+			'profile' => R::findOne('profile', 'user_id = ?', array($user->id)),
+			'gender' => array('M' => '男', 'F' => '女')
+		));
+		$this->load->view('footer');
+	}
 }
