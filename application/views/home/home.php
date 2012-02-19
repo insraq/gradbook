7757ef83c5205f5357c92c041bdeb5bd186f9ab1
insraq <ruoysun@gmail.com>
2012-02-19 21:53:07 +0800

    fix bug

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -6,7 +6,7 @@
 </div>
 
 <?php if (empty($user->photo)): ?>
-<div class="warning">
+<div class="alert">
 	你还没有上传照片，照片将会印在毕业纪念册上，快点点击 <a href="<?php echo site_url('user/profile'); ?>" class="btn">修改资料</a> 上传一张大于1000像素的正方形照片吧！
 </div>
 <?php endif; ?>
