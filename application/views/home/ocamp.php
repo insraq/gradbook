72e6983d47ef4dbf76cc51945142592e04452008
insraq <ruoysun@gmail.com>
2012-02-20 12:02:18 +0800

    Upgrade db structure

--- application/views/home/ocamp.php
+++ application/views/home/ocamp.php
@@ -6,6 +6,9 @@
 </div>
 <?php endif; ?>
 
+<h3>08 Ocamp<h3>
+<p>08 OCamp 让我们结识了大学中的第一批朋友，而今天，我们即将毕业，希望这个向OCamp致敬的专题，能让大家回到那些年，回忆起那些事。</p>
+
 <?php foreach ($group as $k => $v): ?>
 	<a href="<?php echo site_url('home/filter/ocamp_big/' . urlencode($k)); ?>" class="btn btn-primary ocamp"><?php echo $k; ?></a>
 	<?php foreach ($v as $s): ?>
