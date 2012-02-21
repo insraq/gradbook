579f622e8481113ccb733247bfd0873096337271
insraq <ruoysun@gmail.com>
2012-02-21 18:17:24 +0800

    Doulist improvement

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,7 +7,7 @@ class Home extends CI_Controller {
 		$user = $this->login->require_login();
 
 		$this->load->helper('astro');
-		$users = R::find('profile');
+		$users = R::find('profile', 'faculty IS NOT NULL');
 		$this->load->view('header');
 		$this->load->view('home/home', array(
 			'users' => $users,
