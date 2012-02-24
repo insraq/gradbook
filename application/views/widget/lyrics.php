1d7cf7232780e113176256395a3ebcab4272472a
insraq <ruoysun@gmail.com>
2012-02-24 15:36:26 +0800

    Deploy

--- application/views/widget/lyrics.php
+++ application/views/widget/lyrics.php
@@ -1,11 +1,5 @@
 <?php echo nav(3);?>
 
-<?php if (empty($profile->photo)): ?>
-<div class="alert">
-	你还没有上传照片，照片将会印在毕业纪念册上，快点点击 <a href="<?php echo site_url('user/profile'); ?>" class="btn">修改资料</a> 上传一张大于1000像素的正方形照片吧！
-</div>
-<?php endif; ?>
-
 <h2>我的歌词帖</h2>
 <p>
 	<img src="<?php echo base_url('upload/lyrics/' . $user->id . '.png'); ?>" alt="" />
