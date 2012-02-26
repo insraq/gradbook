4559a9b8ce44c0efcf60cc999fef8040d4b5058e
insraq <ruoysun@gmail.com>
2012-02-26 16:23:40 +0800

    Deploy

--- application/controllers/feed.php
+++ application/controllers/feed.php
@@ -11,7 +11,7 @@ class Feed extends CI_Controller {
 			'user' => $user,
 			'profile' => R::findOne('profile', 'user_id = ?', array($user->id)),
 			'gender' => array('M' => '男', 'F' => '女'),
-			'comment' => R::find('comment', '1 ORDER BY id DESC')
+			'comment' => R::find('comment', '1 ORDER BY love ASC, id DESC LIMIT 20')
 		));
 		$this->load->view('footer');
 	}
