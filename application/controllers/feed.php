edbc6e124f3f4df7f5435f796acef17519725509
insraq <ruoysun@gmail.com>
2012-02-26 16:17:09 +0800

    Deploy

--- application/controllers/feed.php
+++ application/controllers/feed.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Feed extends CI_Controller {
 
 	public function index()
 	{
@@ -8,7 +8,7 @@ class Home extends CI_Controller {
 		// $this->load->helper('astro');
 		$this->load->view('header');
 		$this->load->view('feed/index', array(
-			'users' => $users,
+			'user' => $user,
 			'profile' => R::findOne('profile', 'user_id = ?', array($user->id)),
 			'gender' => array('M' => '男', 'F' => '女'),
 			'comment' => R::find('comment', '1 ORDER BY id DESC')
