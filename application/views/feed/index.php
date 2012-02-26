2f897d0cfb063a58f5f4d2d603fe23d04730e31d
insraq <ruoysun@gmail.com>
2012-02-26 16:31:40 +0800

    Deploy

--- application/views/feed/index.php
+++ application/views/feed/index.php
@@ -3,7 +3,7 @@
 <h2>路边社</h2>
 <ul class="comment">
 	<?php foreach ($comment as $c): ?>
-	<li>
+	<li <?php if ($c->love == 'public'): ?>class="love"<?php endif; ?>>
 		<a href="<?php echo site_url('home/view/' . $c->from_user); ?>"><img src="<?php echo R::findOne('profile', 'user_id = ?', array($c->from_user))->photo ? base_url('upload/thumb_' . (R::findOne('profile', 'user_id = ?', array($c->from_user))->photo)) : base_url('asset/img/avatar.png'); ?>" alt="" class="photo-small photo-right" /></a>
 		<div class="meta">
 			<a href="<?php echo site_url('home/view/' . $c->from_user); ?>" class="bubble">来自：<?php echo R::load('user', $c->from_user)->name; ?></a>
