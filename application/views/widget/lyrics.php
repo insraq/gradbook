4a9bd6c518b9541edc2028ad15dc0bb99d715b32
insraq <ruoysun@gmail.com>
2012-03-13 22:23:54 +0800

    Deploy

--- application/views/widget/lyrics.php
+++ application/views/widget/lyrics.php
@@ -4,10 +4,10 @@
 	<img src="<?php echo base_url("upload/lyrics/{$file}.png"); ?>" alt="" />
 </p>
 <p>
-	<a target="_blank" href="http://widget.renren.com/dialog/forward?url=<?php echo urlencode(site_url('widget/lyrics')); ?>&title=<?php echo urlencode('毕业纪念册之我的歌词帖'); ?>&images=<?php echo urlencode(base_url("upload/lyrics/{$file}.png")); ?>">
+	<a target="_blank" href="http://widget.renren.com/dialog/forward?url=<?php echo urlencode(site_url('widget/lyrics')); ?>&title=<?php echo urlencode('我的歌词帖'); ?>&images=<?php echo urlencode(base_url("upload/lyrics/{$file}.png")); ?>">
 		<img src="<?php echo base_url('asset/img/renren_share.png'); ?>" alt="" />
 	</a>
-	<a target="_blank" href="http://service.weibo.com/share/share.php?url=<?php echo urlencode(site_url('widget/lyrics')); ?>&title=<?php echo urlencode('毕业纪念册之我的歌词帖：' . $lyrics); ?>&pic=<?php echo urlencode(base_url("upload/lyrics/{$file}.png")); ?>">
+	<a target="_blank" href="http://service.weibo.com/share/share.php?url=<?php echo urlencode(site_url('widget/lyrics')); ?>&title=<?php echo urlencode('#我的歌词帖# ' . $lyrics); ?>&pic=<?php echo urlencode(base_url("upload/lyrics/{$file}.png")); ?>">
 		<img src="<?php echo base_url('asset/img/weibo.gif'); ?>" alt="" />
 	</a>
 </p>
--- application/views/widget/one_sentence.php
+++ application/views/widget/one_sentence.php
@@ -4,10 +4,10 @@
 	<img src="<?php echo base_url("upload/one_sentence/{$file}.png"); ?>" alt="" />
 </p>
 <p>
-	<a target="_blank" href="http://widget.renren.com/dialog/forward?url=<?php echo urlencode(site_url('widget/one')); ?>&title=<?php echo urlencode('毕业纪念册之一句话证明读过中大'); ?>&images=<?php echo urlencode(base_url("upload/one_sentence/{$file}.png")); ?>">
+	<a target="_blank" href="http://widget.renren.com/dialog/forward?url=<?php echo urlencode(site_url('widget/one')); ?>&title=<?php echo urlencode('一句话证明读过中大'); ?>&images=<?php echo urlencode(base_url("upload/one_sentence/{$file}.png")); ?>">
 		<img src="<?php echo base_url('asset/img/renren_share.png'); ?>" alt="" />
 	</a>
-	<a target="_blank" href="http://service.weibo.com/share/share.php?url=<?php echo urlencode(site_url('widget/one')); ?>&title=<?php echo urlencode('毕业纪念册之#一句话证明读过中大#：' . $one_sentence); ?>&pic=<?php echo urlencode(base_url("upload/one_sentence/{$file}.png")); ?>">
+	<a target="_blank" href="http://service.weibo.com/share/share.php?url=<?php echo urlencode(site_url('widget/one')); ?>&title=<?php echo urlencode('#一句话证明读过中大# ' . $one_sentence); ?>&pic=<?php echo urlencode(base_url("upload/one_sentence/{$file}.png")); ?>">
 		<img src="<?php echo base_url('asset/img/weibo.gif'); ?>" alt="" />
 	</a>
 </p>
