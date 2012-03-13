a0335dda2dae44f6cb9182065591503ee8e8e13f
insraq <ruoysun@gmail.com>
2012-03-13 21:34:40 +0800

    Deploy

--- application/views/widget/regret.php
+++ application/views/widget/regret.php
@@ -1,6 +1,6 @@
 <?php echo nav(6);?>
 <h2>大学遗憾的事情</h2>
-<form action="<?php echo site_url('widget/one_sentence');?>" method="post" class="well">
+<form action="<?php echo site_url('widget/regret_result');?>" method="post" class="well">
 	<p>
 		<input type="text" name="sentence" class="span11" placeholder="大学遗憾的事情">
 	</p>
