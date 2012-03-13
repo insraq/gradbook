51a1890aee87b61edbf763c223174852feed7d51
insraq <ruoysun@gmail.com>
2012-03-13 21:08:32 +0800

    Deploy

--- application/views/widget/lyrics.php
+++ application/views/widget/lyrics.php
@@ -1,7 +1,7 @@
 <?php echo nav(4);?>
 <h2>我的歌词帖</h2>
 <p>
-	<img src="<?php echo base_url('upload/lyrics/{$file}.png'); ?>" alt="" />
+	<img src="<?php echo base_url("upload/lyrics/{$file}.png"); ?>" alt="" />
 </p>
 <p>
 	<a target="_blank" href="http://widget.renren.com/dialog/forward?url=<?php echo urlencode(site_url('widget/lyrics')); ?>&title=<?php echo urlencode('毕业纪念册之我的歌词帖'); ?>&images=<?php echo urlencode(base_url("upload/lyrics/{$file}.png")); ?>">
