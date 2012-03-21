c333b248d2fa15b2fd59bf65b5f0f91cc15cb25b
insraq <ruoysun@gmail.com>
2012-03-21 23:27:51 +0800

    Deploy

---
+++ application/views/home/editor.php
@@ -0,0 +1,52 @@
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>姓名</th>
+			<th>印象最深的时刻</th>
+			<th>？！</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($profile as $p): ?>
+		<tr>
+			<td><?php echo $p->user->name; ?></td>
+			<td><?php echo $p->moment; ?></td>
+			<td><?php echo $p->comment1; ?>？<?php echo $p->comment2; ?>！</td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>姓名</th>
+			<th>一句话证明</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($sentence as $s): ?>
+		<tr>
+			<td><?php echo $s->user->name; ?></td>
+			<td><?php echo $s->text; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>姓名</th>
+			<th>后悔</th>
+		</tr>
+	</thead>
+	<tbody>
+	<?php foreach ($regret as $r): ?>
+		<tr>
+			<td><?php echo $r->user->name; ?></td>
+			<td><?php echo $r->text; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
